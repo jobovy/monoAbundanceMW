1f3094374c58ffec1e83c56bdc7fcd503134852c
Jo Bovy <bovy@ias.edu>
2012-01-10 19:15:19 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup #, Extension
+import os, os.path
+import re
+
+longDescription= ""
+
+
+setup(name='monoAbundanceMW',
+      version='1.',
+      description='mono-abundance MW',
+      author='Jo Bovy',
+      author_email='bovy@ias.edu',
+      license='New BSD',
+      long_description=longDescription,
+      url='https://github.com/jobovy/monoAbundanceMW',
+      package_dir = {'monoAbundanceMW/': ''},
+      packages=['monoAbundanceMW'],
+      package_data={'monoAbundanceMW': ['data/*.sav']},
+      dependency_links = ['https://github.com/esheldon/fitsio/tarball/master#egg=fitsio'],
+      install_requires=['numpy','fitsio']
+      )
