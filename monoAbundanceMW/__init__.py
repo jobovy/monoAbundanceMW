662bd255eab21abb560a83e0a41a96bb62013875
Jo Bovy <bovy@ias.edu>
2012-01-10 20:23:13 -0500

    add sigmaz

--- monoAbundanceMW/__init__.py
+++ monoAbundanceMW/__init__.py
@@ -38,24 +38,149 @@ def abundanceDist(feh,afe,z=None):
 
     """
     #First determine whether this point lies within the fit range
+    if numpy.sum((numpy.fabs(results['feh']-feh) <= _DFEH/2.)\
+                     *(numpy.fabs(results['afe']-afe) <= _DAFE/2.)) == 0.:
+        return 0.
+    #Then find the relevant bin
+    indx= (numpy.fabs(results['feh']-feh) <= _DFEH/2.)\
+        *(numpy.fabs(results['afe']-afe) <= _DAFE/2.)
+    if z is None:
+        return results['mass'][indx][0]
+    elif isinstance(z,(list,numpy.ndarray)):
+        hz= results['hz'][indx][0]
+        if z[0] is None and not z[1] is None:
+            return results['mass'][indx][0]*(1.-numpy.exp(-z[1]/hz))
+        elif z[1] is None and not z[0] is None:
+            return results['mass'][indx][0]*numpy.exp(-z[0]/hz)
+        elif z[0] is None and z[1] is None:
+            return results['mass'][indx][0]
+        else:
+            return results['mass'][indx][0]*(numpy.exp(-z[0]/hz)-numpy.exp(-z[1]/hz))
+    else:
+        hz= results['hz'][indx][0]
+        return results['mass'][indx][0]/2./hz*math.exp(-numpy.fabs(z)/hz)
+
+def hz(feh,afe,err=False):
+    """
+    NAME:
+
+       hz
+
+    PURPOSE:
+
+       return the vertical scale height as a function of feh,afe
+
+    INPUT:
+
+       feh - metallicity
+
+       afe - alpha-enhancement
+
+       err= (default: False) if True, also return error
+
+    OUTPUT:
+
+       vertical scale height in pc (, +uncertainty)
+
+    HISTORY:
+
+       2012-01-10 - Written - Bovy (IAS/@Tucson)
+
+    """
+    #First determine whether this point lies within the fit range
     if numpy.sum((numpy.fabs(results['feh']-feh) < _DFEH/2.)\
                      *(numpy.fabs(results['afe']-afe) < _DAFE/2.)) == 0.:
         return 0.
     #Then find the relevant bin
-    indx= (numpy.fabs(results['feh']-feh) < _DFEH/2.)\
-        *(numpy.fabs(results['afe']-afe) < _DAFE/2.)
-    if z is None:
-        return results['mass'][indx]
-    elif isinstance(z,(list,numpy.ndarray)):
-        hz= results['hz'][indx]
-        if z[0] is None and not z[1] is None:
-            return results['mass'][indx]*(1.-numpy.exp(-z[1]/hz))
-        elif z[1] is None and not z[0] is None:
-            return results['mass'][indx]*numpy.exp(-z[0]/hz)
-        elif z[0] is None and z[1] is None:
-            return results['mass'][indx]
-        else:
-            return results['mass'][indx]*(numpy.exp(-z[0]/hz)-numpy.exp(-z[1]/hz))
+    indx= (numpy.fabs(results['feh']-feh) <= _DFEH/2.)\
+        *(numpy.fabs(results['afe']-afe) <= _DAFE/2.)
+    if err:
+        return (results['hz'][indx][0],results['hz_err'][indx][0])
     else:
-        hz= results['hz'][indx]
-        return results['mass'][indx]/2./hz*math.exp(-numpy.fabs(z)/hz)
+        return results['hz'][indx][0]
+
+def hr(feh,afe,err=False):
+    """
+    NAME:
+
+       hr
+
+    PURPOSE:
+
+       return the radial scale length as a function of feh,afe
+
+    INPUT:
+
+       feh - metallicity
+
+       afe - alpha-enhancement
+
+       err= (default: False) if True, also return error
+
+    OUTPUT:
+    
+       radial scale length in kpc (, +uncertainty)
+
+    HISTORY:
+
+       2012-01-10 - Written - Bovy (IAS/@Tucson)
+
+    """
+    #First determine whether this point lies within the fit range
+    if numpy.sum((numpy.fabs(results['feh']-feh) <= _DFEH/2.)\
+                     *(numpy.fabs(results['afe']-afe) <= _DAFE/2.)) == 0.:
+        return 0.
+    #Then find the relevant bin
+    indx= (numpy.fabs(results['feh']-feh) <= _DFEH/2.)\
+        *(numpy.fabs(results['afe']-afe) <= _DAFE/2.)
+    if err:
+        return (results['hr'][indx][0],results['hr_err'][indx][0])
+    else:
+        return results['hr'][indx][0]
+
+def sigmaz(feh,afe,z=1000.,R=8.,err=False):
+    """
+    NAME:
+
+       sigmaz
+
+    PURPOSE:
+
+       return the vertical velocity dispersion at height Z as a function of feh,afe
+
+    INPUT:
+
+       feh - metallicity
+
+       afe - alpha-enhancement
+
+       z= (default: 1000) height [pc]
+
+       R= (default 8) Galactocentric radius [kpc]
+
+       err= (default: False) if True, also return error
+
+    OUTPUT:
+    
+       vertical velocity dispersion at height Z
+
+    HISTORY:
+
+       2012-01-10 - Written - Bovy (IAS/@Tucson)
+
+    """
+    #First determine whether this point lies within the fit range
+    if numpy.sum((numpy.fabs(results['feh']-feh) <= _DFEH/2.)\
+                     *(numpy.fabs(results['afe']-afe) <= _DAFE/2.)) == 0.:
+        return 0.
+    #Then find the relevant bin
+    indx= (numpy.fabs(results['feh']-feh) <= _DFEH/2.)\
+        *(numpy.fabs(results['afe']-afe) <= _DAFE/2.)
+    if err:
+        raise NotImplementedError("Err for sigmaz not implemented yet")
+        return (results['hr'][indx][0],results['hr_err'][indx][0])
+    else:
+        d= (z-results['zmedian'][indx])/1000.
+        return (results['sz'][indx][0]+d*results['p1'][indx][0]\
+            +d**2.*results['p2'][indx][0])*math.exp(-(R-8.)/results['hsz'][indx][0])
+
